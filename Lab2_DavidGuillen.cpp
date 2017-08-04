0f6860c337bf8ef14378cc03352fa08eb8eb28a9
guillendiaz <guillendiazd@unitec.edu>
2017-08-04 15:23:23 -0600

    San

--- Lab2_DavidGuillen.cpp
+++ Lab2_DavidGuillen.cpp
@@ -15,12 +15,17 @@ char** GenerarBarcosJ1(char**, int);
 //Generar barcos del jugador1
 
 char** GenerarBarcosJ2(char**, int);
+//Generar barcos del jugador2
 
+bool VerificarCoordenada(char**, int, int);
+// Verifica la coordenada ingresada
 int main(){
 	int size = 8;
 	char** Matrix = GenerarMatriz(8);
 	int TerminarJuego = 0;
 	int Turno = 1;
+	int cantidadcaracter = 0;
+	int CantidadBarcosJ1 = 0;
 	//while(TerminarTurno = 0){
 		Matrix = Tablero(Matrix, size);	
 		//ImpTablero(Matrix, 0, 0, size, size);
@@ -29,9 +34,28 @@ int main(){
 		//ImpTablero(MatrixJ1, 0, 0, 8, 8);
 		char** MatrixJ2 = GenerarMatriz(8);
 		MatrixJ2 = GenerarBarcosJ2(MatrixJ2, 8);
-		ImpTablero(MatrixJ2, 0, 0, 8, 8);
+		//ImpTablero(MatrixJ2, 0, 0, 8, 8);
 		if(Turno == 1){
-			
+			cout<<"Turno J1"<<endl;
+			int i;
+			int j;
+			cout<<"Ingrese Coordenada i: ";
+			cin>>i;
+			cout<<endl;
+			cout<<"Ingrese Coordenada j: ";
+			cin>>j;
+			bool VerCoor = VerificarCoordenada(MatrixJ1, i, j);
+			if(bool == true){
+				cout<<"Ha Disparado en una Parte de Barco"<<endl;
+				cantidadcaracter++;
+			}else{
+				cout<<"Ha Fallado"<<endl;
+			}
+			if(cantidadcaracter == 3){
+				CantidadBarcosJ1++;
+				catidadcaracter = 0;
+			}
+			Turno = 2;
 		}else if(Turno == 2){
 				
 		}//if/else if()
@@ -100,11 +124,11 @@ char** GenerarBarcosJ2(char** tablero, int size){
 		for(int j=0; j<size; j++){
 			if(i==0 && j>0 && j<4){
 				tablero[i][j] = '#';
-			}if else(i==3 && j>4 && j<size-1){
+			}else if(i==3 && j>4 && j<=size-1){
 				tablero[i][j] = '#';
-			}if else(j==1 && i>4 && i<size-1){
-				tablero[i][j] = '#':
-			}if else(j==4 && i>3 && i<6){
+			}else if(j==1 && i>4 && i<=size-1){
+				tablero[i][j] = '#';
+			}else if(j==4 && i>3 && i<7){
 				tablero[i][j] = '#';
 			}else{
 				tablero[i][j] = '*';
@@ -113,3 +137,17 @@ char** GenerarBarcosJ2(char** tablero, int size){
 	}//for1()
 	return tablero;
 }//GenerarBarcosJ2()
+
+bool VerificarCoordenada(char** tablero, int x, int y){
+	for(int i=0; i<size; i++){
+		for(int j=0; j<size; j++){
+			if(i==x && j==y){
+				if(tablero[i][j] == '#';){
+					return true;
+				}else{
+					return false;
+				}//if/else()
+			}
+		}//for2()
+	}//for1()
+}//VerificarCoordenada()
