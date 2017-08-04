f184f9892442688b170fd5b24d70b32e24eabd8d
guillendiaz <guillendiazd@unitec.edu>
2017-08-04 16:14:41 -0600

    Roku

--- Lab2_DavidGuillen.cpp
+++ Lab2_DavidGuillen.cpp
@@ -23,15 +23,28 @@ bool VerificarCoordenada(char**, int, int, int);
 char** NuevoTablero(char**, int, int, int);
 //Pone donde disparo el jugador
 
+int VerificarBarcoJ1(char**, int, int, int);
+//Verifica las vidas de los barcos
+
+int VerificarBarcoJ2(char**, int, int, int);
+
 int main(){
 	int size = 8;
 	char** Matrix = GenerarMatriz(8);
 	int TerminarJuego = 0;
 	int Turno = 1;
-	int cantidadcaracter = 0;
 	int CantidadBarcosJ1 = 0;
-	int cantidadcaracter2 = 0;
 	int CantidadBarcosJ2 = 0;
+	int VerBarcoJ1 = 0;
+	int BarJ1_1 = 0;
+	int BarJ1_2 = 0;
+	int BarJ1_3 = 0;
+	int BarJ1_4 = 0;
+	int VerBarcoJ2 = 0;
+	int BarJ2_1 = 0;
+	int BarJ2_2 = 0;
+	int BarJ2_3 = 0;
+	int BarJ2_4 = 0;
 	char** Matrix2 =GenerarMatriz(8);
 	Matrix2 = Tablero(Matrix2, size);
 	Matrix = Tablero(Matrix, size);	
@@ -54,13 +67,25 @@ int main(){
 			bool VerCoor = VerificarCoordenada(MatrixJ1, i, j, size);
 			if(VerCoor == true){
 				cout<<"Ha Disparado en una Parte de Barco"<<endl;
-				cantidadcaracter++;
 			}else{
 				cout<<"Ha Fallado"<<endl;
 			}
-			if(cantidadcaracter == 3){
+			VerBarcoJ1 = VerificarBarcoJ1(MatrixJ1, i, j, size);
+			if(VerBarcoJ1 == 1){
+				BarJ1_1++;
+				VerBarcoJ1 = 0;
+			}else if(VerBarcoJ1 == 2){
+				BarJ1_2++;
+				VerBarcoJ1 = 0;
+			}else if(VerBarcoJ1 == 3){
+				BarJ1_3++;
+				VerBarcoJ1 = 0;
+			}else if(VerBarcoJ1 == 4){
+				BarJ1_4++;
+				VerBarcoJ1 = 0;
+			}
+			if(BarJ1_1 == 3 || BarJ1_2 ==3 || BarJ1_3 == 3 || BarJ1_4 == 3){
 				CantidadBarcosJ1++;
-				cantidadcaracter = 0;
 			}
 			Turno = 2;
 			Matrix = NuevoTablero(Matrix, i, j, size);
@@ -78,13 +103,25 @@ int main(){
 			bool VerCoor2 = VerificarCoordenada(MatrixJ2, i, j, size);
 			if(VerCoor2 == true){
 				cout<<"Ha Disparado en una Parte de Barco"<<endl;
-				cantidadcaracter2++;
 			}else{
 				cout<<"Ha Fallado"<<endl;
 			}
-			if(cantidadcaracter2 == 3){
+			VerBarcoJ2 = VerificarBarcoJ2(MatrixJ2, i, j, size);
+			if(VerBarcoJ2 == 1){
+				BarJ2_1++;
+				VerBarcoJ2 = 0;
+			}else if(VerBarcoJ2 == 2){
+				BarJ2_2++;
+				VerBarcoJ2 = 0;
+			}else if(VerBarcoJ2 == 3){
+				BarJ2_3++;
+				VerBarcoJ2 = 0;
+			}else if(VerBarcoJ2 == 4){
+				BarJ2_4++;
+				VerBarcoJ2 = 0;
+			}
+			if(BarJ2_1 == 3 || BarJ2_2 ==3 || BarJ2_3 == 3 || BarJ2_4 == 3){
 				CantidadBarcosJ2++;
-				cantidadcaracter2 = 0;
 			}
 			Turno = 1;
 			Matrix2 = NuevoTablero(Matrix2, i, j, size);
@@ -194,3 +231,39 @@ char** NuevoTablero(char** tablero, int x, int y, int size){
 	}//for1()
 	return tablero;
 }//NuevoTablero()
+
+int VerificarBarcoJ1(char** tablero, int x, int y, int size){
+	for(int i=0; i<size; i++){
+		for(int j=0; j<size; j++){
+			if(i == 1 && j>0 && j<4 && tablero[i][j] == '#'){
+				return 1;
+			}else if(i == size-1 && j>0 && j<4 && tablero[i][j] == '#'){
+				return 2;
+			}else if(j == 4 && i>2 && i<6 && tablero[i][j] == '#'){
+				return 3;
+			}else if(j == size-1 && i>1 && i<5 &&tablero[i][j] == '#'){
+				return 4;
+			}else{
+				return 0;
+			}//if/else if()
+		}//for2()
+	}//for1()
+}//VerificarBarcosJ1()
+
+int VerificarBarcoJ2(char** tablero, int x, int y, int size){
+	for(int i=0; i<size; i++){
+		for(int j=0; j<size; j++){
+			if(i == 1 && j>0 && j<4 && tablero[i][j] == '#'){
+				return 1;
+			}else if(i == size-1 && j>0 && j<4 && tablero[i][j] == '#'){
+				return 2;
+			}else if(j == 4 && i>2 && i<6 && tablero[i][j] == '#'){
+				return 3;
+			}else if(j == size-1 && i>1 && i<5 &&tablero[i][j] == '#'){
+				return 4;
+			}else{
+				return 0;
+			}//if/else if()
+		}//for2()
+	}//for1()
+}//VerificarBarcosJ1()
